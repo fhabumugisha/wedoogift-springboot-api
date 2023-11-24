ecb826c90cb293e6195d074af050a2f632cc00c0
fabriceboris <fabriceboris@gmail.com>
2023-11-24 17:38:30 +0100

    Add validation error handler

--- src/main/java/com/wedogift/backend/controllers/GlobalExeptionHandler.java
+++ src/main/java/com/wedogift/backend/controllers/GlobalExeptionHandler.java
@@ -1,12 +1,13 @@
 package com.wedogift.backend.controllers;
 
-import com.wedogift.backend.exceptions.CompanyNonFoundException;
-import com.wedogift.backend.exceptions.ErrorDto;
-import com.wedogift.backend.exceptions.NotEnoughBalanceException;
-import com.wedogift.backend.exceptions.UserNotFoundException;
+import com.wedogift.backend.exceptions.*;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,5 +28,27 @@ public class GlobalExeptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDto> handleException(Exception ex) {
         return  ResponseEntity.internalServerError().body(ErrorDto.builder().message(ex.getMessage()).status(HttpStatus.INTERNAL_SERVER_ERROR.value()).build() );
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+   public ResponseEntity<ValidationErrorResponse> onConstraintValidationException(
+            ConstraintViolationException e) {
+        ValidationErrorResponse error = ValidationErrorResponse.builder().build();
+        for (ConstraintViolation violation : e.getConstraintViolations()) {
+            error.violations().add(
+                    new Violation(violation.getPropertyPath().toString(), violation.getMessage()));
+        }
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse>  onMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        ValidationErrorResponse error =  ValidationErrorResponse.builder().build();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            error.violations().add(
+                    new Violation(fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        return ResponseEntity.badRequest().body(error);
     }
 }
